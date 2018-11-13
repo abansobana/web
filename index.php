e7bebab6223ba482e291e2cd8b29b054b574bd19
abansobana <45006261+abansobana@users.noreply.github.com>
2018-11-13 21:14:06 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,46 @@
+ <html>
+<head>
+<title>Membuat Biodata Sederhana</title>
+</head>
+<body>
+<form action="#" style="width: 700px"class="posisi";>
+<fieldset class="h"/>
+<table style="width: 600px;">
+<body style="background-color:blue"> </body>
+
+<tr>
+<td rowspan="10" width="100px">
+<img src="POTOS.jpg" width="150px" height="200px" border="5">
+</td>
+</tr>
+<center>
+<font size="25px">
+<b>BIODATA</b>
+</font>
+</center>
+<tr>
+<td><b>NAMA</b></td>
+<td>:   Aban Sobana</td>
+</tr>
+<tr>
+<td><b>NIK</b></td>
+<td>:   123456789</td>
+</tr>
+<tr>
+<td><b>TTL</b></td>
+<td>:   Jakarta, 08/08/2018</td>
+</tr>
+<tr>
+<td><b>ALAMAT</b></td>
+<td>:   Jkarta</td>
+</tr>
+<tr>
+<td><b>JENIS KELAMIN</b></td>
+<td>:   Laki - Laki</td>
+</tr>
+<tr>
+<td><b>AGAMA</b></td>
+<td>:   Islam</td>
+</tr>
+</body>
+</html>
